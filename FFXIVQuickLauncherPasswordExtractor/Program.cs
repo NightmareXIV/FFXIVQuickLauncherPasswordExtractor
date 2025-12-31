1fc6de156f183550683518c1960d700a2b9f4671
Limiana <5073202+Limiana@users.noreply.github.com>
2026-01-01 01:49:52 +0300

    Update

--- FFXIVQuickLauncherPasswordExtractor/Program.cs
+++ FFXIVQuickLauncherPasswordExtractor/Program.cs
@@ -14,14 +14,37 @@ namespace FFXIVQuickLauncherPasswordExtractor
             Console.WriteLine("FFXIV Quick launcher password extractor");
             while (true)
             {
-                Console.WriteLine("Type your username to attempt to view password. Type \"exit\" to exit.");
+                Console.WriteLine("Type your username to attempt to view password. Type \"exit\" to exit, \"list\" - to list all available credentials.");
                 var a = Console.ReadLine();
                 if (a.Equals("exit", StringComparison.OrdinalIgnoreCase)) break;
+                if(a.Equals("list"))
+                {
+                    try
+                    {
+                        var credentials = CredentialManager.EnumerateICredentials().Where(x => x.UserName != "" || x.TargetName != "");
+
+                        Console.WriteLine($"Found {credentials.Count()} credentials:\n");
+
+                        foreach(var cred in credentials)
+                        {
+                            Console.WriteLine($"Target: {cred.TargetName}");
+                            Console.WriteLine($"Username: {cred.UserName}");
+                            Console.WriteLine($"Password: {CredentialManager.GetCredentials(cred.TargetName)?.Password}");
+                            Console.WriteLine();
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                }
                 try
                 {
                     var credentials = CredentialManager.GetCredentials($"FINAL FANTASY XIV-{a.ToLower()}") 
-                        ?? CredentialManager.GetCredentials($"XIVLAUNCHER-{a.ToLower()}") 
-                        ?? CredentialManager.GetCredentials($"XIVLAUNCHERCN-{a.ToLower()}");
+                        ?? CredentialManager.GetCredentials($"XIVLAUNCHER-{a.ToLower()}")
+                        ?? CredentialManager.GetCredentials($"XIVLAUNCHERCN-{a.ToLower()}")
+                        ?? CredentialManager.GetCredentials($"{a.ToLower()}")
+                        ?? CredentialManager.GetCredentials($"{a}");
                     if(credentials == null)
                     {
                         Console.WriteLine("Not found");
